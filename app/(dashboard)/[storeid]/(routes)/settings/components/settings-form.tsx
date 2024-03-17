b11cb671d4be49c6597e0e4367697143ce8adf1e
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-03-17 01:51:36 +0100

    feat: implement dashboard settings api integration

--- app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -1,15 +1,20 @@
 "use client"
 
+import axios from "axios"
 import * as z from "zod"
 import { useState } from "react"
+import toast from "react-hot-toast"
 import { Trash } from "lucide-react"
 import { Store } from "@prisma/client"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useParams, useRouter } from "next/navigation"
 
 import { Input } from "@/components/ui/input"
+import { useOrigin } from "@/hooks/use-origin"
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
+import { ApiAlert } from "@/components/ui/api-alert"
 import { Separator } from "@/components/ui/separator"
 import {
   Form,
@@ -19,6 +24,7 @@ import {
   FormLabel,
   FormMessage
 } from "@/components/ui/form"
+import { AlertModal } from "@/components/modals/alert-modal"
 
 interface SettingsFormProps {
   initialData: Store
@@ -32,8 +38,12 @@ const formSchema = z.object({
 type settingsFormValues = z.infer<typeof formSchema>
 
 export function SettingsForm({ initialData }: SettingsFormProps) {
+  const params = useParams()
+  const router = useRouter()
+  const origin = useOrigin()
+
   const [open, setOpen] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const form = useForm<settingsFormValues>({
     resolver: zodResolver(formSchema),
@@ -41,21 +51,55 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
   })
 
   const onSubmit = async (values: settingsFormValues) => {
-    console.log("🚀 ~ onSubnit ~ values:", values)
+    try {
+      setLoading(true)
+
+      const response = await axios.patch(
+        `/api/stores/${params.storeid}`,
+        values
+      )
+      router.refresh()
+      toast.success("Store updated successfully")
+    } catch (error) {
+      toast.error("somthing went wrong, Try again!")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeid}`)
+      router.refresh()
+      router.push("/")
+      toast.success("Store deleted Successfully")
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first")
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
   }
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading
           title="Settings"
           description="Manaage your store preferences"
         />
         <Button
-          disabled={isLoading}
+          disabled={loading}
           variant="destructive"
           size="sm"
-          onClick={() => {}}
+          onClick={() => setOpen(true)}
         >
           <Trash className="w-4 h-4" />
         </Button>
@@ -75,7 +119,7 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
                   <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input
-                      disabled={isLoading}
+                      disabled={loading}
                       placeholder="Store name"
                       {...field}
                     />
@@ -85,11 +129,17 @@ export function SettingsForm({ initialData }: SettingsFormProps) {
               )}
             />
           </div>
-          <Button disabled={isLoading} className="ml-auto" type="submit">
+          <Button disabled={loading} className="ml-auto" type="submit">
             Save changes
           </Button>
         </form>
       </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeid}`}
+        variant="public"
+      />
     </>
   )
 }
